25e1e8848b63ebbc99aa6089fa7e86263cbde50a
NoelGamaliel <techmbala@gmail.com>
2024-10-20 21:52:07 -0700

    My first commit for mySiteWeb

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+    // echo 'Welcome';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>myWebSite</title>
+
+    <link rel="stylesheet" href="public/assets/style/style.css">
+</head>
+<body>
+    <main class="backColor">
+        <header>
+            <div class="container">
+                <nav>
+                    <div class="logo">
+                        <a href="http://">Logo</a>
+                    </div>
+                    <ul>
+                        <li><a href="http://">Home</a></li>
+                        <li><a href="http://">About</a></li>
+                        <li><a href="http://">Services</a></li>
+                        <li><a href="http://">Contact</a></li>
+                    </ul>
+                    <div class="btn-don">
+                        <a href="http://">Don</a>
+                    </div> 
+                </nav>                
+            </div>
+        </header>
+
+        <section class="banner">
+            <div class="container">
+                <div class="banner-content">
+                    <h1>Welcome to my WebSite</h1>
+
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum in expedita eum error maxime? Nihil, fugiat? Nostrum debitis incidunt earum nihil eius quisquam necessitatibus mollitia?</p>
+                </div>                
+            </div>
+        </section>
+
+    </main>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
